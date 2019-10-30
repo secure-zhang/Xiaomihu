86b00c99cb63bca6380a0a832166a0ff8bd01e36
xiaomi <17635035787@163.com>
2019-10-30 21:15:40 +0800

    start

---
+++ Xiaomihu/manager.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from __init__ import app
+from flask import render_template
+
+@app.route('/',methods=['GET','POST'])
+def test():
+    return render_template('index.html')
+if __name__ == '__main__':
+    app.run()
